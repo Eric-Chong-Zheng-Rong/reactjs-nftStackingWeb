011fdfe3ea2f78970b7c54e6feff9c9e57c8af16
Eric-Chong <eric.chong.6868@gmail.com>
2022-09-08 03:28:47 +0800

    Updates

---
+++ precache-manifest.72eebbdd8ee55f8c5841f1caf348c7f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f0ef70a86988e0581a25721122ff57",
+    "url": "/reactjs-nftStackingWeb/index.html"
+  },
+  {
+    "revision": "1bb554c74c18e142dc9f",
+    "url": "/reactjs-nftStackingWeb/static/css/2.5e069dfe.chunk.css"
+  },
+  {
+    "revision": "a6f54070d3cca194a59b",
+    "url": "/reactjs-nftStackingWeb/static/css/main.43af11b1.chunk.css"
+  },
+  {
+    "revision": "1bb554c74c18e142dc9f",
+    "url": "/reactjs-nftStackingWeb/static/js/2.3615ba6a.chunk.js"
+  },
+  {
+    "revision": "fe47ad810bd9c94268c892546ed7b118",
+    "url": "/reactjs-nftStackingWeb/static/js/2.3615ba6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6f54070d3cca194a59b",
+    "url": "/reactjs-nftStackingWeb/static/js/main.e8ea4e18.chunk.js"
+  },
+  {
+    "revision": "435fa0cecfe0742cfe42",
+    "url": "/reactjs-nftStackingWeb/static/js/runtime-main.8852760c.js"
+  }
+]);
